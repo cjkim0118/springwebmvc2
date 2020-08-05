a754c601690ec0989945554d83c99bb6dab36080
cjkim0118 <cjkim0118@gmail.com>
2020-08-05 11:03:25 +0900

    최종

--- src/main/java/org/zerock/config/RootConfig.java
+++ src/main/java/org/zerock/config/RootConfig.java
@@ -1,8 +1,40 @@
 package org.zerock.config;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class RootConfig {
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
+
+@Configuration
+@ComponentScan(basePackages= {"org.zerock.sample"})
+@MapperScan(basePackages = {"org.zerock.mapper"})
+public class RootConfig {
+	
+	@Bean
+	public DataSource datasource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:49161:XE");
+		hikariConfig.setUsername("book_ex");
+		hikariConfig.setPassword("book_ex");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		
+		return dataSource;
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception{
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(datasource());
+		return(SqlSessionFactory) sqlSessionFactory.getObject();
+	}
 }
